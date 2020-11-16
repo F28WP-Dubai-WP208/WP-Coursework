5522a8621b7c42cd92f90cc56202ceddda1759b9
aimee <aml10@hw.ac.uk>
2020-11-16 11:12:42 +0400

    updated

---
+++ WP-CW/Streetscapes/public/options.js
@@ -0,0 +1,21 @@
+function openOption() {
+    document.getElementById("Options").style.display = "block";
+    
+}
+
+function closeOption(){
+    document.getElementById("Options").style.display = "none";
+
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    
+}
+function closeForm(){
+    document.getElementById("myForm").style.display = "none";
+
+}
+
+
+
